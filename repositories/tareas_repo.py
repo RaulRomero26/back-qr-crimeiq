77509dcb70c7e302b8b2426f628c398a0256e065
Raul Romero <link_2642@hotmail.com>
2024-05-06 21:25:28 -0600

    update: fecha de asignacion de la tarea

--- repositories/tareas_repo.py
+++ repositories/tareas_repo.py
@@ -4,6 +4,7 @@ def crearTarea(data):
     try:
         # Insertar los datos recibidos desde la aplicación frontend en la colección
         result = collection_tareas.insert_one(data)
+        print(result)
         print('ID del documento insertado:', result.inserted_id)
         return {'success': True, 'message': 'Tarea creada exitosamente.'}
     except Exception as e:
