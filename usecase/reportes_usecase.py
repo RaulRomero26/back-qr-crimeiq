5d433ff25555286cdbd51e8ca63c96bda6282ae4
RaulRomero26 <link_2642@hotmail.com>
2024-05-16 13:54:54 -0600

    fix: guardar reporte

--- usecase/reportes_usecase.py
+++ usecase/reportes_usecase.py
@@ -6,7 +6,7 @@ class ReportesUsecase:
     def __init__(self, qr_repository):
         self.qr_repository = qr_repository
 
-    def guardar_reporte(reporte_data):
+    def guardar_reporte(self,reporte_data):
         return reportes_repository.guardar_reporte(reporte_data)
     
     def guardar_alerta(self,alerta_data):
