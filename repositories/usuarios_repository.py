21ea6bb469c9338afdbd12e3ea036c5f8dcdc956
Raul Romero <link_2642@hotmail.com>
2024-06-22 14:33:46 -0600

    fix ident

--- repositories/usuarios_repository.py
+++ repositories/usuarios_repository.py
@@ -93,50 +93,50 @@ class UsuariosRepository:
     #         }
 
     def actualizar_usuario(self, usuario_data):
-    try:
-        # Obtener el ID del usuario y convertirlo a ObjectId
-        user_id = usuario_data.get('_id')
-        user_id = ObjectId(user_id)  # Convierte la cadena a ObjectId
+        try:
+            # Obtener el ID del usuario y convertirlo a ObjectId
+            user_id = usuario_data.get('_id')
+            user_id = ObjectId(user_id)  # Convierte la cadena a ObjectId
 
-        # Obtener el documento del usuario actual desde la base de datos
-        current_user = collection_Usu.find_one({'_id': user_id})
+            # Obtener el documento del usuario actual desde la base de datos
+            current_user = collection_Usu.find_one({'_id': user_id})
 
-        # Verificar si el documento del usuario existe
-        if not current_user:
+            # Verificar si el documento del usuario existe
+            if not current_user:
+                return {
+                    'success': False,
+                    'message': 'Usuario no encontrado.'
+                }
+
+            # Actualizar los datos del usuario en la colección
+            print('viene de inactivar:', usuario_data)
+            update_data = {key: value for key, value in usuario_data.items() if key not in ['password', 'Foto', '_id']}
+
+            # Solo hashear si se proporciona una nueva contraseña y no está vacía
+            if 'password' in usuario_data and usuario_data['password'] and usuario_data['password'] != "":
+                password = usuario_data['password']
+                hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                update_data['password'] = hashed_password.decode('utf-8')
+
+            # Incluir 'Foto' en los datos de actualización si está presente en usuario_data
+            if 'Foto' in usuario_data:
+                update_data['Foto'] = usuario_data['Foto']
+
+            # Manejar el campo 'activo'
+            update_data['activo'] = usuario_data.get('activo', '').lower() == 'true'
+
+            print(user_id)
+            
+            collection_Usu.update_one({'_id': user_id}, {'$set': update_data}, upsert=True)
+            return {
+                'success': True,
+                'message': 'Usuario actualizado exitosamente.'
+            }
+        except Exception as e:
             return {
                 'success': False,
-                'message': 'Usuario no encontrado.'
+                'error': str(e)
             }
-
-        # Actualizar los datos del usuario en la colección
-        print('viene de inactivar:', usuario_data)
-        update_data = {key: value for key, value in usuario_data.items() if key not in ['password', 'Foto', '_id']}
-
-        # Solo hashear si se proporciona una nueva contraseña y no está vacía
-        if 'password' in usuario_data and usuario_data['password'] and usuario_data['password'] != "":
-            password = usuario_data['password']
-            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            update_data['password'] = hashed_password.decode('utf-8')
-
-        # Incluir 'Foto' en los datos de actualización si está presente en usuario_data
-        if 'Foto' in usuario_data:
-            update_data['Foto'] = usuario_data['Foto']
-
-        # Manejar el campo 'activo'
-        update_data['activo'] = usuario_data.get('activo', '').lower() == 'true'
-
-        print(user_id)
-        
-        collection_Usu.update_one({'_id': user_id}, {'$set': update_data}, upsert=True)
-        return {
-            'success': True,
-            'message': 'Usuario actualizado exitosamente.'
-        }
-    except Exception as e:
-        return {
-            'success': False,
-            'error': str(e)
-        }
 
 
 usuarios_repository = UsuariosRepository()
